44bcc2ff84d34bfffd4f10bbc19770abfa0553e9
Nesrine <nesrine.larbi@mail.concordia.ca>
2026-01-19 12:26:23 -0500

    feat: add Firebase integration with configuration and dependencies

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA3rubUq06rVm1fRoxC4BJOCEJSihPRts8",
+  authDomain: "barbershop-app-a3657.firebaseapp.com",
+  projectId: "barbershop-app-a3657",
+  storageBucket: "barbershop-app-a3657.firebasestorage.app",
+  messagingSenderId: "488932919037",
+  appId: "1:488932919037:web:59d045eb788e909389e094"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
